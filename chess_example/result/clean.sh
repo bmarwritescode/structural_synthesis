26205a602e2a9594c43eb12284f1f2e1280bd4da
Ben Mariano <benmariano13@gmail.com>
2018-07-18 12:56:21 -0400

    cleaning a bit

---
+++ chess_example/result/clean.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+rm -rf java*
+rm -rf sk*
+rm -rf tmp/*
+rm -rf *.txt log/*.txt
--- chess_example/run.sh
+++ chess_example/run.sh
@@ -1,11 +1,8 @@
-DP=../structural_synthesis/chess2
+DP=`pwd`
+JSK="/Users/grumpy/Research/java-sketch"
 
-cd ../../java-sketch/
+cd ${JSK}
 
-# ./jsk.sh model/lang/Object.java model/util/List.java model/util/ArrayList.java ${DP}/ChessboardTest.java ${DP}/Player.java ${DP}/Position.java ${DP}/Piece.java ${DP}/Move.java ${DP}/Chessboard.java ${DP}/TwoDArray.java ${DP}/ChessboardBuilder.java ${DP}/Chess.java ${DP}/Tower.java ${DP}/Queen.java ${DP}/Bishop.java ${DP}/Pawn.java ${DP}/Knight.java ${DP}/King.java ${DP}/SuperPawn.java ${DP}/Advisor.java
-
-./jsk.sh model/lang/Object.java model/util/List.java model/util/ArrayList.java ${DP}/ChessboardTest.java ${DP}/Player.java ${DP}/Position.java ${DP}/Piece.java ${DP}/Move.java ${DP}/Chessboard.java ${DP}/TwoDArray.java ${DP}/ChessboardBuilder.java ${DP}/Chess.java ${DP}/Tower.java ${DP}/Queen.java ${DP}/Bishop.java ${DP}/Pawn.java ${DP}/Knight.java ${DP}/King.java ${DP}/SuperPawn.java ${DP}/Advisor_syn.java
-
-# ./jsk.sh model/lang/Object.java axioms/util/ArrayList.java model/lang/String.java model/lang/CharacterSequence.java model/lang/Number.java model/lang/Integer.java model/org/junit ${DP}/ChessboardTest.java ${DP}/Player.java ${DP}/Position.java ${DP}/Piece.java ${DP}/Move.java ${DP}/Chessboard.java ${DP}/TwoDArray.java ${DP}/ChessboardBuilder.java ${DP}/Chess.java ${DP}/Tower.java ${DP}/Queen.java ${DP}/Bishop.java ${DP}/Pawn.java ${DP}/Knight.java ${DP}/King.java ${DP}/SuperPawn.java ${DP}/Advisor_syn.java  --no-lib
+${JSK}/jsk.sh model/lang/Object.java model/util/List.java model/util/ArrayList.java ${DP}/ChessboardTest.java ${DP}/Player.java ${DP}/Position.java ${DP}/Piece.java ${DP}/Move.java ${DP}/Chessboard.java ${DP}/TwoDArray.java ${DP}/ChessboardBuilder.java ${DP}/Chess.java ${DP}/Tower.java ${DP}/Queen.java ${DP}/Bishop.java ${DP}/Pawn.java ${DP}/Knight.java ${DP}/King.java ${DP}/SuperPawn.java ${DP}/Advisor_syn.java --out_dir ${DP}/result/
 
 cd ${DP}
